78490104dc42b3c56739f41b0f27c91dfa82476c
Aluizio Developer <aluiziodeveloper@gmail.com>
2025-03-06 22:18:33 -0300

    feat: add DTOs in CreateCategory use case

--- src/core/category/application/usecases/create_category.py
+++ src/core/category/application/usecases/create_category.py
@@ -1,6 +1,17 @@
+from dataclasses import dataclass
 from uuid import UUID
 from src.core.category.application.usecases.exceptions import InvalidCategory
 from src.core.category.domain.category import Category
+
+@dataclass
+class CreateCategoryRequest:
+    name: str
+    description: str = ""
+    is_active: bool = True
+
+@dataclass
+class CreateCategoryResponse:
+    id: UUID
 
 class InMemoryCategoryRepository:
     def __init__(self, categories=None):
@@ -9,19 +20,20 @@ class InMemoryCategoryRepository:
     def save(self, category):
         self.categories.append(category)
 
-# input: name, description, is_active
-# output: category_id (UUID)
-def create_category(
-        repository: InMemoryCategoryRepository,
-        name: str,
-        description: str = "",
-        is_active: bool = True
-    ) -> UUID:
-    try:
-        category = Category(name=name, description=description, is_active=is_active)
-    except ValueError as error:
-        raise InvalidCategory(error)
+class CreateCategoryUseCase:
+    def __init__(self, repository: InMemoryCategoryRepository):
+        self.repository = repository
 
-    repository.save(category)
+    def execute(self, request: CreateCategoryRequest) -> CreateCategoryResponse:
+        try:
+            category = Category(
+                name=request.name,
+                description=request.description,
+                is_active=request.is_active
+            )
+        except ValueError as error:
+            raise InvalidCategory(error)
 
-    return category.id
+        self.repository.save(category)
+
+        return category.id
--- src/core/category/tests/application/usecases/test_create_category.py
+++ src/core/category/tests/application/usecases/test_create_category.py
@@ -1,42 +1,29 @@
 from unittest.mock import MagicMock
 from uuid import UUID
-from src.core.category.application.usecases.create_category import InMemoryCategoryRepository, create_category
 import pytest
 
+from src.core.category.application.usecases.create_category import CreateCategoryRequest, CreateCategoryUseCase, InMemoryCategoryRepository
 from src.core.category.application.usecases.exceptions import InvalidCategory
 
 class TestCreateCategory:
-    def test_create_active_category_with_name_and_description(self):
+    def test_create_category_with_valid_data(self):
         mock_repository = MagicMock(InMemoryCategoryRepository)
-        output = create_category(
-            repository=mock_repository,
-            name="Movies",
-            description="Movie category",
-            is_active=True
+        usecase = CreateCategoryUseCase(repository=mock_repository)
+        request = CreateCategoryRequest(
+            name="Filme",
+            description="Categoria para filmes",
+            is_active=True,
         )
+        category_id = usecase.execute(request)
 
-        assert isinstance(output, UUID)
+        assert category_id is not None
+        assert isinstance(category_id, UUID)
         assert mock_repository.save.called is True
 
-    def test_create_inactive_category_with_name_and_description(self):
-        mock_repository = MagicMock(InMemoryCategoryRepository)
-        output = create_category(
-            repository=mock_repository,
-            name="Movies",
-            description="Movie category",
-            is_active=False
-        )
-
-        assert isinstance(output, UUID)
-        assert mock_repository.save.called is True
-
-    def test_when_input_is_invalid_then_raise_exception(self):
+    def test_create_category_with_invalid_data(self):
+        usecase = CreateCategoryUseCase(repository=MagicMock(InMemoryCategoryRepository))
         with pytest.raises(InvalidCategory, match="name cannot be empty") as exc_info:
-            create_category(
-                repository=MagicMock(InMemoryCategoryRepository),
-                name="",
-                description="Movie category"
-            )
+            usecase.execute(CreateCategoryRequest(name=""))
 
         assert exc_info.type == InvalidCategory
         assert str(exc_info.value) == "name cannot be empty"
