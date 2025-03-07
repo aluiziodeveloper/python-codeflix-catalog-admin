b173181350c6c3c6b04979c9aa5ec1c5f8dad3d4
Aluizio Developer <aluiziodeveloper@gmail.com>
2025-03-06 21:22:50 -0300

    feat: add exception handling

--- src/core/category/application/usecases/create_category.py
+++ src/core/category/application/usecases/create_category.py
@@ -1,10 +1,14 @@
 from uuid import UUID
+from src.core.category.application.usecases.exceptions import InvalidCategory
 from src.core.category.domain.category import Category
 
 # input: name, description, is_active
 # output: category_id (UUID)
-def create_category(name, description, is_active = True) -> UUID:
-    category = Category(name=name, description=description, is_active=is_active)
+def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+    try:
+        category = Category(name=name, description=description, is_active=is_active)
+    except ValueError as error:
+        raise InvalidCategory(error)
 
     # TODO: persist category
 
--- src/core/category/tests/application/usecases/test_create_category.py
+++ src/core/category/tests/application/usecases/test_create_category.py
@@ -1,5 +1,8 @@
 from uuid import UUID
 from src.core.category.application.usecases.create_category import create_category
+import pytest
+
+from src.core.category.application.usecases.exceptions import InvalidCategory
 
 class TestCreateCategory:
     def test_create_active_category_with_name_and_description(self):
@@ -19,3 +22,10 @@ class TestCreateCategory:
         )
 
         assert isinstance(output, UUID)
+
+    def test_when_input_is_invalid_then_raise_exception(self):
+        with pytest.raises(InvalidCategory, match="name cannot be empty") as exc_info:
+            create_category("", "Movie category")
+
+        assert exc_info.type == InvalidCategory
+        assert str(exc_info.value) == "name cannot be empty"
