d4034d1081cc5b5be0ae46d4eae3e06cf3d67b55
Aluizio Developer <aluiziodeveloper@gmail.com>
2025-03-06 21:50:42 -0300

    feat: add in memory category repository

--- src/core/category/application/usecases/create_category.py
+++ src/core/category/application/usecases/create_category.py
@@ -2,14 +2,26 @@ from uuid import UUID
 from src.core.category.application.usecases.exceptions import InvalidCategory
 from src.core.category.domain.category import Category
 
+class InMemoryCategoryRepository:
+    def __init__(self, categories=None):
+        self.categories = categories or []
+
+    def save(self, category):
+        self.categories.append(category)
+
 # input: name, description, is_active
 # output: category_id (UUID)
-def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+def create_category(
+        repository: InMemoryCategoryRepository,
+        name: str,
+        description: str = "",
+        is_active: bool = True
+    ) -> UUID:
     try:
         category = Category(name=name, description=description, is_active=is_active)
     except ValueError as error:
         raise InvalidCategory(error)
 
-    # TODO: persist category
+    repository.save(category)
 
     return category.id
--- src/core/category/tests/application/usecases/test_create_category.py
+++ src/core/category/tests/application/usecases/test_create_category.py
@@ -1,31 +1,42 @@
+from unittest.mock import MagicMock
 from uuid import UUID
-from src.core.category.application.usecases.create_category import create_category
+from src.core.category.application.usecases.create_category import InMemoryCategoryRepository, create_category
 import pytest
 
 from src.core.category.application.usecases.exceptions import InvalidCategory
 
 class TestCreateCategory:
     def test_create_active_category_with_name_and_description(self):
+        mock_repository = MagicMock(InMemoryCategoryRepository)
         output = create_category(
+            repository=mock_repository,
             name="Movies",
             description="Movie category",
             is_active=True
         )
 
         assert isinstance(output, UUID)
+        assert mock_repository.save.called is True
 
     def test_create_inactive_category_with_name_and_description(self):
+        mock_repository = MagicMock(InMemoryCategoryRepository)
         output = create_category(
+            repository=mock_repository,
             name="Movies",
             description="Movie category",
             is_active=False
         )
 
         assert isinstance(output, UUID)
+        assert mock_repository.save.called is True
 
     def test_when_input_is_invalid_then_raise_exception(self):
         with pytest.raises(InvalidCategory, match="name cannot be empty") as exc_info:
-            create_category("", "Movie category")
+            create_category(
+                repository=MagicMock(InMemoryCategoryRepository),
+                name="",
+                description="Movie category"
+            )
 
         assert exc_info.type == InvalidCategory
         assert str(exc_info.value) == "name cannot be empty"
